bfa8cd65edc65407fd74fd5791dc725a2f4d7101
roberthparry <robert.h.parry@gmail.com>
2018-10-04 02:28:53 +0100

    reshuffle ship.go

--- battleships-service/ship.go
+++ battleships-service/ship.go
@@ -6,6 +6,30 @@ type Ship struct {
 	Hits   int
 }
 
+//Destroyer a destroyer class
+type Destroyer struct {
+	Ship
+}
+
+//Battleship a destroyer class
+type Battleship struct {
+	Ship
+}
+
+//NewDestroyer create instance of a destroyer
+func NewDestroyer() *Destroyer {
+	ship := &Destroyer{}
+	ship.Length = 4
+	return ship
+}
+
+//NewBattleship create instance of a destroyer
+func NewBattleship() *Battleship {
+	ship := &Battleship{}
+	ship.Length = 5
+	return ship
+}
+
 //IsDestroyed returns true if the ship is destroyed
 func (ship *Ship) IsDestroyed() bool {
 	return ship.Hits == ship.Length
