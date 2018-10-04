b17d183237cac7805e91f76d2eba8453c71ea6a9
roberthparry <robert.h.parry@gmail.com>
2018-10-04 02:29:52 +0100

    allow seeding to happen automatically

--- battleships-service/randomizer.go
+++ battleships-service/randomizer.go
@@ -5,17 +5,25 @@ import (
 	"time"
 )
 
-//RandomInit initialise randomizer
-func RandomInit() {
+var seeded = false
+
+func randomInit() {
 	rand.Seed(time.Now().UnixNano())
+	seeded = true
 }
 
 //RandomDirection returns DirectionDown or DirectionRight
 func RandomDirection() Direction {
+	if !seeded {
+		randomInit()
+	}
 	return Direction(rand.Intn(2))
 }
 
 //RandomPosition returns a random position to start laying out the ship of the given length
 func RandomPosition(length int) int {
+	if !seeded {
+		randomInit()
+	}
 	return rand.Intn(GameboardGridSize - length + 1)
 }
